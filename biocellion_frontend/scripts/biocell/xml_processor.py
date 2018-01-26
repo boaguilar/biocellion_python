88b2a2f3bc8d73c401dbb0f0e58120fcdc063d40
Curtis Larsen <larsen@dixie.edu>
2018-01-26 12:13:01 -0700

    improved documentation and errors for XMLProcessor

--- biocellion_frontend/scripts/biocell/xml_processor.py
+++ biocellion_frontend/scripts/biocell/xml_processor.py
@@ -3,6 +3,13 @@ import xml.etree.ElementTree as ET
 import re
     
 class XMLProcessor:
+    """
+    Translates the XML file in input_filename and stores the resulting
+    XML in output_filename.  Any tag value or element text the pattern
+    VAR_(.*) is replaced with values[ \1 ].
+    The construct does all the work, so just instantiate an object to
+    get the desired results.
+    """
 
     def __init__( self, input_filename, output_filename, values ):
         self.mTree = None
@@ -28,14 +35,22 @@ class XMLProcessor:
                 match = re.search( '^VAR_(.*)$', value )
                 if match and match.group( 1 ) in self.mValues:
                     elem.set( name, str( self.mValues[ match.group( 1 ) ] ) )
-                # s += " " + str( name ) + '="' + str( value ) + '"'
+                elif match and match.group( 1 ) not in self.mValues:
+                    msg = "ERROR: VAR_" + str( match.group( 1 ) )
+                    msg += " found in the template."
+                    msg += str( match.group( 1 ) )
+                    msg += " is not in the substitution values."
+                    raise Exception( msg )
             if elem.text:
                 match = re.search( '^VAR_(.*)$', elem.text )
                 if match and match.group( 1 ) in self.mValues:
                     elem.text = str( self.mValues[ match.group( 1 ) ] )
-                # s +=  " value: " + str( elem.text )
-            # print( s )
-                
+                elif match and match.group( 1 ) not in self.mValues:
+                    msg = "ERROR: VAR_" + str( match.group( 1 ) )
+                    msg += " found in the template."
+                    msg += str( match.group( 1 ) )
+                    msg += " is not in the substitution values."
+                    raise Exception( msg )
         return
     
     def saveXML( self ):
